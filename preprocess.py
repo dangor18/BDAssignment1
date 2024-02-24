d74674e357741635404e32a429485f6ce9b823ba
Joe Whales <joewhales0@gmail.com>
2024-02-24 17:23:13 +0200

    refactored code

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from faker import Faker
+import csv
+
 
 def ratings():
     # Read the original CSV file
@@ -54,6 +56,29 @@ def tags():
     print(final_df)
     return final_df
 
+def transform_csv(input_csv, output_csv):
+    user_books = {}
+
+    # Read input CSV and aggregate book IDs for each user
+    with open(input_csv, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            user_id = row['user_id']
+            book_id = row['book_id']
+            if user_id in user_books:
+                user_books[user_id].append(book_id)
+            else:
+                user_books[user_id] = [book_id]
+
+    # Write aggregated data to output CSV
+    with open(output_csv, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['user_id', 'book_ids'])
+        for user_id, book_ids in user_books.items():
+            writer.writerow([user_id, ','.join(book_ids)])
+
+
+
 if __name__ == "__main__":
     book_ratings_df = ratings()
     book_tags_df = tags()
@@ -62,3 +87,10 @@ if __name__ == "__main__":
     print(final_df)
     # write data to a new csv file
     final_df.to_csv('data/merged.csv', index=False)
+    # Specify input and output CSV file paths
+    input_csv = 'data/to_read.csv'
+    output_csv = 'to_read_merged.csv'
+
+    # Transform CSV
+    transform_csv(input_csv, output_csv)
+
