0aecfffaeab4edef23008b25c5fd57d200a5b484
dangor18 <dangordon.za@gmail.com>
2024-02-25 10:30:08 +0200

    preprocess generates user dataframe with list of ratings and list of toreads

--- preprocess.py
+++ preprocess.py
@@ -55,7 +55,7 @@ def book_to_tags():
     return final_df
 
 def user_to_book_ratings():
-    # Assuming the necessary CSV files are located in the 'data' directory and named accordingly.
+    # read files
     ratings_df = pd.read_csv('data/ratings.csv').head(10000)
     user_names_df = pd.read_csv('user_data.csv')
     ratings_df = pd.merge(ratings_df, user_names_df, on='user_id')
@@ -81,14 +81,48 @@ def user_to_book_ratings():
         "rating": row['rating']
     }, axis=1).tolist())
 
-    # create a new DataFrame with book_id and ratings
-    new_df = pd.DataFrame({'user_id': grouped.index, 'ratings': grouped.values})
-    #print(new_df)
-    new_df.to_csv("test.csv")
-    #return result
+    grouped_df = pd.DataFrame({'user_id': grouped.index, 'ratings': grouped.values})
+    #print(grouped_df)
+    #grouped_df.to_csv("test.csv")
+    return grouped_df
 
 def user_to_read():
-    return
+    # read files
+    read_df = pd.read_csv('data/to_read.csv')
+    ratings_df = pd.read_csv('data/ratings.csv').head(10000)
+    
+    # only get users to_read data who you're taking from ratings for consistency
+    ratings_df = ratings_df.drop(['book_id', 'rating'], axis=1)
+    read_df = pd.merge(ratings_df, read_df, on='user_id')
+    
+    # add user names
+    user_names_df = pd.read_csv('user_data.csv')
+    read_df = pd.merge(read_df, user_names_df, on='user_id')
+    
+    books_df = book_to_tags()
+    merged_df = read_df.merge(books_df, on='book_id')
+    
+    grouped = merged_df.groupby('user_id').apply(lambda x: x.apply(lambda row: {
+        "book": {
+            "book_id": row['book_id'],
+            "goodreads_book_id": row['goodreads_book_id'],
+            "work_id": row['work_id'],
+            "authors": row['authors'],
+            "title": row['title'],
+            "isbn": row['isbn'],
+            "isbn13": row['isbn13'],
+            "language_code": row['language_code'],
+            "average_rating": row['average_rating'],
+            "ratings_count": row['ratings_count'],
+            "image_url": row['image_url'],
+            "tags": row["tags"]
+        }
+    }, axis=1).tolist())
+
+    grouped_df = pd.DataFrame({'user_id': grouped.index, 'to_read': grouped.values})
+    #print(grouped_df)
+    #grouped_df.to_csv("test.csv")
+    return grouped_df
 
 def transform_csv(input_csv, output_csv):
     user_books = {}
@@ -112,7 +146,8 @@ def transform_csv(input_csv, output_csv):
             writer.writerow([user_id, ','.join(book_ids)])
 
 if __name__ == "__main__":
-    user_to_book_ratings()
+    user_final_df = user_to_book_ratings().merge(user_to_read(), on='user_id', how='left')
+    print(user_final_df)
     # book to ratings objects dataframe
     book_ratings_df = book_to_user_ratings()
     #print(book_ratings_df)
@@ -121,11 +156,12 @@ if __name__ == "__main__":
     #print(book_tags_df)
 
     # merge the two
-    final_df = book_tags_df.merge(book_ratings_df, on='book_id')
+    book_final_df = book_tags_df.merge(book_ratings_df, on='book_id')
     #print(final_df)
-    
     # write data to a new csv file
-    final_df.to_csv('data/merged.csv', index=False)
+    book_final_df.to_csv('data/merged.csv', index=False)
+
+    user_final_df = user_to_book_ratings().merge(user_to_read(), on='user_id', how='left')
     # specify input and output CSV file paths
     input_csv = 'data/to_read.csv'
     output_csv = 'to_read_merged.csv'
