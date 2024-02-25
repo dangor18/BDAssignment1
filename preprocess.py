4449b8b607e04fae6e60d184399fe6aaf0f97197
dangor18 <dangordon.za@gmail.com>
2024-02-25 11:32:30 +0200

    started to_read, didnt finish

--- preprocess.py
+++ preprocess.py
@@ -24,6 +24,31 @@ def book_to_user_ratings():
     #print(new_df)
     return new_df
 
+""""
+def book_to_read():
+    # read files
+    read_df = pd.read_csv('data/to_read.csv')
+    ratings_df = pd.read_csv('data/ratings.csv').head(10000)
+    
+    # only get users to_read data who you're taking from ratings for consistency
+    ratings_df = ratings_df.drop(['book_id', 'rating'], axis=1)
+    read_df = pd.merge(ratings_df, read_df, on='user_id')
+    
+    # group by book_id and aggregate ratings for each book with user objects
+    grouped = read_df.groupby('book_id').apply(lambda x: x.apply(lambda row: {
+        "user": {
+            "user_id": row['user_id'],
+            "user_name": row['user_name']
+        },
+        "rating": row['rating']
+    }, axis=1).tolist())
+
+    # create a new DataFrame with book_id and ratings
+    new_df = pd.DataFrame({'book_id': grouped.index, 'ratings': grouped.values})
+    #print(new_df)
+    return new_df
+"""
+
 def book_to_tags():
     books_df = pd.read_csv('data/books.csv')
     # split authors
