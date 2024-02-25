8ccf57a1af5bf867d6b3bf14a2d50e760f58c45c
dangor18 <dangordon.za@gmail.com>
2024-02-25 09:03:24 +0200

    preprocess uses correct id and writes to merged.csv

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,13 @@
 import pandas as pd
-from faker import Faker
 import csv
 
 def ratings():
-    # Read the original CSV file
-    df = (pd.read_csv('data/ratings.csv')).head(100000)
+    # read the original CSV file
+    df = (pd.read_csv('data/ratings.csv')).head(10000)
     user_names_df = pd.read_csv('user_data.csv')
     df = pd.merge(df, user_names_df, on='user_id')
     #df.rename(columns={'book_id': 'work_id'}, inplace=True)
-    # Group by book_id and aggregate ratings for each book with user objects
+    # group by book_id and aggregate ratings for each book with user objects
     grouped = df.groupby('book_id').apply(lambda x: x.apply(lambda row: {
         "user": {
             "user_id": row['user_id'],
@@ -17,7 +16,7 @@ def ratings():
         "rating": row['rating']
     }, axis=1).tolist())
 
-    # Create a new DataFrame with book_id and ratings
+    # create a new DataFrame with book_id and ratings
     new_df = pd.DataFrame({'book_id': grouped.index, 'ratings': grouped.values})
     #print(new_df)
     return new_df
@@ -44,10 +43,10 @@ def tags():
     book_tags_df = pd.read_csv('data/book_tags.csv')
     tags_df = pd.read_csv('data/tags.csv')
 
-    # Merge book_tags with tags to get the tag names along with their IDs for each book
+    # merge book_tags with tags to get the tag names along with their IDs for each book
     book_tags_merged_df = pd.merge(book_tags_df, tags_df, on='tag_id')
 
-    # Group tags by book_id and aggregate into a list of dictionaries
+    # group tags by book_id and aggregate into a list of dictionaries
     book_tags_grouped = book_tags_merged_df.groupby('goodreads_book_id').apply(lambda x: x[['tag_id', 'tag_name']].apply(lambda row: {'tag_id': row['tag_id'], 'tag_name': row['tag_name']}, axis=1).tolist()).reset_index(name='tags')
     
     final_df = pd.merge(books_df, book_tags_grouped, on='goodreads_book_id')
@@ -57,7 +56,7 @@ def tags():
 def transform_csv(input_csv, output_csv):
     user_books = {}
 
-    # Read input CSV and aggregate book IDs for each user
+    # read input CSV and aggregate book IDs for each user
     with open(input_csv, 'r', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -68,7 +67,7 @@ def transform_csv(input_csv, output_csv):
             else:
                 user_books[user_id] = [book_id]
 
-    # Write aggregated data to output CSV
+    # write aggregated data to output CSV
     with open(output_csv, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['user_id', 'book_ids'])
@@ -76,33 +75,35 @@ def transform_csv(input_csv, output_csv):
             writer.writerow([user_id, ','.join(book_ids)])
 
 if __name__ == "__main__":
+    # book to ratings objects dataframe
     book_ratings_df = ratings()
-    print(book_ratings_df[book_ratings_df['book_id'] == 258])
+    print(book_ratings_df)
+    # books to tags object dataframe
     book_tags_df = tags()
-    print(book_tags_df[book_tags_df['book_id'] == 258])
+    print(book_tags_df)
 
+    # merge the two
     final_df = book_tags_df.merge(book_ratings_df, on='book_id')
-    print(final_df[final_df['book_id'] == 258])
-    """
+    print(final_df)
+    
+    
     # write data to a new csv file
     final_df.to_csv('data/merged.csv', index=False)
-    # Specify input and output CSV file paths
+    # specify input and output CSV file paths
     input_csv = 'data/to_read.csv'
     output_csv = 'to_read_merged.csv'
 
-    # Transform CSV
+    # transform CSV
     transform_csv(input_csv, output_csv)
     df = pd.read_csv('data/ratings.csv')
 
-    # Group by user_id and aggregate book_id and rating as a list of dictionaries
+    # group by user_id and aggregate book_id and rating as a list of dictionaries
     grouped = df.groupby('user_id').apply(lambda x: x[['book_id', 'rating']].apply(lambda y: {'book': y['book_id'], 'rating': y['rating']}, axis=1).tolist())
 
-    # Create a new DataFrame with user_id and book_ratings
+    # create a new DataFrame with user_id and book_ratings
     new_df = pd.DataFrame({'user_id': grouped.index, 'book_ratings': grouped.values})
 
-    # Save the new DataFrame to a CSV file
+    # save the new DataFrame to a CSV file
     new_df.to_csv('ratings_grouped_by_user.csv', index=False)
     
     print("Preprocessing completed.")
-    """
-
