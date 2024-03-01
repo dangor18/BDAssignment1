6ae069dd5780205d0e69e108799c3e381dcc3cb4
Jordy Kafwe <jordykafwe@gmail.com>
2024-03-01 13:21:51 +0200

    Commit env local

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-mongoimport --host mongodb --port 27017 --db goodbooks --mode upsert --collection books --type json --file /mongo-seed/books.json --jsonArray
-mongoimport --host mongodb --port 27017 --db goodbooks --mode upsert --collection users --type json --file /mongo-seed/users.json --jsonArray
+mongoimport --uri "mongodb+srv://admin:uGvwGdcT60QG75rs@cluster0.ce8456i.mongodb.net/goodbooks?retryWrites=true&w=majority" --collection books --type json --file /mongo-seed/books.json --jsonArray
+
+mongoimport --uri "mongodb+srv://admin:uGvwGdcT60QG75rs@cluster0.ce8456i.mongodb.net/goodbooks?retryWrites=true&w=majority" --collection users --type json --file /mongo-seed/users.json --jsonArray
